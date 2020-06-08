c9d04ba43e0c30324e6858e0294156f2ae19fc1a
EddyBorges <63222336+EddyBorges@users.noreply.github.com>
2020-06-08 12:16:28 -0300

    Atividade 1165

---
+++ Atividade 1165.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int X, cont;
+        for (int i = 0; i < N; i++) {
+        	cont = 0;
+        	X = leitor.nextInt();
+        	for (int j = 2; j < X; j++) {
+        		if (X % j == 0) cont++;
+        	}
+        	if (cont == 0) System.out.println(X + " eh primo");
+        	else System.out.println(X + " nao eh primo");
+        }
+    }
+	
+}
