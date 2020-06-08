61054bbf695265dab7e4cfb171d1d7bd95c5ac3e
EddyBorges <63222336+EddyBorges@users.noreply.github.com>
2020-06-08 12:15:57 -0300

    Atividade 1164

---
+++ Atividade 1164.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int X, soma;
+        for (int i = 0; i < N; i++) {
+        	soma = 0;
+        	X = leitor.nextInt();
+        	for (int j = 1; j < X; j++) {
+        		if (X % j == 0) soma += j;
+        	}
+        	if (soma == X) System.out.println(X + " eh perfeito");
+        	else System.out.println(X + " nao eh perfeito");
+        }
+    }
+	
+}
